67b675eb6605833b6088a7f251ab8cb98a196ca0
nick repasy <nrepasy@gmail.com>
2020-11-27 19:18:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,7 @@ list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective 
 another_language = "Python"
 
 def using_insert(list_of_programming_languages , another_language)
- list_of_programming_languages.to_i
+ list_of_programming_languages.to_s
   list_of_programming_languages.insert(another_language)
   p list_of_programming_languages
 end
