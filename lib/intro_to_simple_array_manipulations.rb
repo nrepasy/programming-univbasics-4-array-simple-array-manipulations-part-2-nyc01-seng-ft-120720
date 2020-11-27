2b792aa65d2a648b96e46c5134a8cf7cf519f4cd
nick repasy <nrepasy@gmail.com>
2020-11-27 19:15:00 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+more_faves = ["sports cars", "flatiron school"]
+
+def using_concat(my_favorite_things , more_faves)
+
+
+ my_favorite_things.concat(more_faves)
+p my_favorite_things
+end
+
+list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+another_language = "Python"
+def using_insert(list_of_programming_languages , another_language)
+ list_of_programming_languages.to_s
+  list_of_programming_languages.insert(another_language)
+  p list_of_programming_languages
+end
