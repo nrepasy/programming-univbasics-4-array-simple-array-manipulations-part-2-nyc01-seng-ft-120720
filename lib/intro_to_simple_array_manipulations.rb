bc3b75de20b6e6ba8038e06bc404b66bdaca5540
nick repasy <nrepasy@gmail.com>
2020-11-27 19:31:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,6 +27,7 @@ end
 instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
 def using_delete(instructors)
   instructors.delete("Steven")
-  p instructors
+  instructors
+end
 end
   
