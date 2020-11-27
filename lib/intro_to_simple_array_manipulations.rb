46c9bf70fb0c00e0c171a203d50994ddee0b09ba
nick repasy <nrepasy@gmail.com>
2020-11-27 19:18:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,19 +1,16 @@
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_faves = ["sports cars", "flatiron school"]
-
 def using_concat(my_favorite_things , more_faves)
-
-
- my_favorite_things.concat(more_faves)
+  my_favorite_things.concat(more_faves)
 p my_favorite_things
 end
 
 
 list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
 another_language = "Python"
-another_language.to_i
+
 def using_insert(list_of_programming_languages , another_language)
- list_of_programming_languages.to_s
+ list_of_programming_languages.to_i
   list_of_programming_languages.insert(another_language)
   p list_of_programming_languages
 end
